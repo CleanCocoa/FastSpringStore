78d3662b433a8ae4c725e86d1fc10effdaced4a0
Christian Tietze <me@christiantietze.de>
2024-02-01 12:17:13 +0100

    add FsprgEmbeddedStore.xcframework downloadable dependency

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FsprgEmbeddedStore",
-            path: "../Carthage/Build/FsprgEmbeddedStore.xcframework"
+            url: "https://github.com/DivineDominion/FsprgEmbeddedStoreMac/releases/download/2.0.0/FsprgEmbeddedStore.xcframework.zip",
+            checksum: "f105f849e252ee77023aa2df99bd043f6624b4fc8748b37947aab8d3b82a3d9c"
         ),
         .target(
             name: "FastSpringStore",
